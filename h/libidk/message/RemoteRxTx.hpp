9956c88bd9ec963fcf8506ba5b05f57e7387beeb
mellic03 <mellic03@gmail.com>
2026-08-02 21:19:03 +1000

    Automated commit

--- h/libidk/message/RemoteRxTx.hpp
+++ h/libidk/message/RemoteRxTx.hpp
@@ -1,10 +1,59 @@
+// #pragma once
+
+// #include "MessageRxTx.hpp"
+
+// struct NET_DatagramSocket;
+// struct NET_Address;
+// struct NET_Datagram;
+
+// namespace idk
+// {
+//     class RemoteRxer: public idk::MessageRxer
+//     {
+//     public:
+//         using MessageRxer::recvMsg;
+
+//         RemoteRxer(uint16_t port);
+//         virtual bool recvMsg(void *data, size_t size) override;
+
+//     private:
+//         const uint32_t mAuthToken;
+//         NET_DatagramSocket *mSocket;
+//         uint16_t mPort;
+//         // size_t mHeaderOffset;
+//         // size_t mPayloadOffset;
+//         uint8_t mBuffer[1500];
+
+//         NET_Datagram *beginRecvMsg();
+//         bool goodRecvMsg(NET_Datagram*);
+//         bool badRecvMsg(NET_Datagram*);
+//     };
+
+
+//     class RemoteTxer: public idk::MessageTxer
+//     {
+//     public:
+//         using MessageTxer::sendMsg;
+
+//         RemoteTxer(const char *hostname, uint16_t dstport);
+//         ~RemoteTxer();
+//         virtual bool sendMsg(const void *data, size_t size) override;
+
+//     private:
+//         const uint32_t mAuthToken;
+//         NET_DatagramSocket *mSocket;
+//         NET_Address *mRemoteAddr;
+//         uint16_t mDstPort;
+//         uint8_t mBuffer[1500];
+//     };
+// }
+
 #pragma once
 
 #include "MessageRxTx.hpp"
 
 struct NET_DatagramSocket;
 struct NET_Address;
-struct NET_Datagram;
 
 namespace idk
 {
@@ -17,16 +66,10 @@ namespace idk
         virtual bool recvMsg(void *data, size_t size) override;
 
     private:
-        const uint32_t mAuthToken;
+        const uint32_t mUdpMagic;
         NET_DatagramSocket *mSocket;
         uint16_t mPort;
-        // size_t mHeaderOffset;
-        // size_t mPayloadOffset;
-        uint8_t mBuffer[1500];
-
-        NET_Datagram *beginRecvMsg();
-        bool goodRecvMsg(NET_Datagram*);
-        bool badRecvMsg(NET_Datagram*);
+        uint8_t mBuf[1500];
     };
 
 
@@ -40,11 +83,11 @@ namespace idk
         virtual bool sendMsg(const void *data, size_t size) override;
 
     private:
-        const uint32_t mAuthToken;
+        const uint32_t mUdpMagic;
         NET_DatagramSocket *mSocket;
         NET_Address *mRemoteAddr;
         uint16_t mDstPort;
-        uint8_t mPayload[1500];
+        uint8_t mBuf[1500];
     };
 }
 
