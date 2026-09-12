a7edd09ed8c9ca9945dd0a3ffff93e94b0572adc
mellic03 <mellic03@gmail.com>
2026-09-13 02:08:47 +1000

    woopee

--- src/platform/SDL3/SDL3Video.cpp
+++ src/platform/SDL3/SDL3Video.cpp
@@ -1,8 +1,17 @@
 #include "libidk/platform/SDL3Video.hpp"
 #include "libidk/log.hpp"
 
-#include <glad/glad.h>
+#define VOLK_IMPLEMENTATION
+#include <volk.h>
+ 
+#define VMA_IMPLEMENTATION
+#include <vk_mem_alloc.h>
+ 
+#include <VkBootstrap.h>
+ 
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+ 
 
 
 idk::SDL3Video::SDL3Video(const char *title, int w, int h)
@@ -17,70 +26,70 @@ idk::SDL3Video::SDL3Video(const char *title, int w, int h)
         VLOG_FATAL("{}", SDL_GetError());
     }
 
-    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE))
-    {
-        VLOG_ERROR("{}", SDL_GetError());
-    }
+    // if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE))
+    // {
+    //     VLOG_ERROR("{}", SDL_GetError());
+    // }
 
-    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4))
-    {
-        VLOG_ERROR("{}", SDL_GetError());
-    }
+    // if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4))
+    // {
+    //     VLOG_ERROR("{}", SDL_GetError());
+    // }
 
-    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6))
-    {
-        VLOG_ERROR("{}", SDL_GetError());
-    }
+    // if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6))
+    // {
+    //     VLOG_ERROR("{}", SDL_GetError());
+    // }
 
-    if (!SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4))
-    {
-        VLOG_ERROR("{}", SDL_GetError());
-    }
+    // if (!SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4))
+    // {
+    //     VLOG_ERROR("{}", SDL_GetError());
+    // }
 
-    if (!SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1))
-    {
-        VLOG_ERROR("{}", SDL_GetError());
-    }
+    // if (!SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1))
+    // {
+    //     VLOG_ERROR("{}", SDL_GetError());
+    // }
 
-    if (!SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,  24))
-    {
-        VLOG_ERROR("{}", SDL_GetError());
-    }
+    // if (!SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,  24))
+    // {
+    //     VLOG_ERROR("{}", SDL_GetError());
+    // }
 
-    if (!SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8))
-    {
-        VLOG_ERROR("{}", SDL_GetError());
-    }
+    // if (!SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8))
+    // {
+    //     VLOG_ERROR("{}", SDL_GetError());
+    // }
 
-    if (!(mWin = SDL_CreateWindow(title, mWidth, mHeight, SDL_WINDOW_OPENGL)))
+    if (!(mWin = SDL_CreateWindow(title, mWidth, mHeight, SDL_WINDOW_VULKAN)))
     {
         VLOG_FATAL("SDL_CreateWindow: {}", SDL_GetError());
     }
 
-    if (!(mGl = SDL_GL_CreateContext((SDL_Window*)mWin)))
-    {
-        VLOG_FATAL("SDL_GL_CreateContext: {}", SDL_GetError());
-    }
+    // if (!(mGl = SDL_GL_CreateContext((SDL_Window*)mWin)))
+    // {
+    //     VLOG_FATAL("SDL_GL_CreateContext: {}", SDL_GetError());
+    // }
 
-    if (!SDL_GL_SetSwapInterval(1))
-    {
-        VLOG_WARN("SDL_GL_SetSwapInterval: {}", SDL_GetError());
-    }
+    // if (!SDL_GL_SetSwapInterval(1))
+    // {
+    //     VLOG_WARN("SDL_GL_SetSwapInterval: {}", SDL_GetError());
+    // }
 
-    int interval;
-    if (!SDL_GL_GetSwapInterval(&interval))
-    {
-        VLOG_WARN("SDL_GL_GetSwapInterval: {}", SDL_GetError());
-    }
-    else
-    {
-        VLOG_INFO("SDL_GL_GetSwapInterval: interval={}", interval);
-    }
+    // int interval;
+    // if (!SDL_GL_GetSwapInterval(&interval))
+    // {
+    //     VLOG_WARN("SDL_GL_GetSwapInterval: {}", SDL_GetError());
+    // }
+    // else
+    // {
+    //     VLOG_INFO("SDL_GL_GetSwapInterval: interval={}", interval);
+    // }
 
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
-    {
-        VLOG_FATAL("gladLoadGLLoader failure");
-    }
+    // if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+    // {
+    //     VLOG_FATAL("gladLoadGLLoader failure");
+    // }
 }
 
 
